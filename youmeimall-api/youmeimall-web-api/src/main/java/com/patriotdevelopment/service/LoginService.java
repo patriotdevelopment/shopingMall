736361e8e5b8380f32691da3d8ab9d586d22a8aa
fengzhongguji <15883788762@163.com>
2019-04-05 21:38:35 +0800

    集成前端

---
+++ youmeimall-api/youmeimall-web-api/src/main/java/com/patriotdevelopment/service/LoginService.java
@@ -0,0 +1,15 @@
+package com.patriotdevelopment.service;
+
+import com.patriotdevelopment.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public interface LoginService {
+
+
+}
