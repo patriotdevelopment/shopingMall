bf6ba50627a551000ac57a9833b0523d62a92bae
fengzhongguji <15883788762@163.com>
2019-04-07 17:16:12 +0800

    搭建消息队列

--- youmeimall-web/src/main/java/com/patriotdevelopment/controller/LoginController.java
+++ youmeimall-web/src/main/java/com/patriotdevelopment/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.patriotdevelopment.controller;
 
+import com.patriotdevelopment.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoginController {
     @RequestMapping("/login")
-  public String login(){
+  public String login(Model model, HttpServletRequest request){
       return "login";
   }
+
 }
